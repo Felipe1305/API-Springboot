b6040133da3ecebdfb93a52ace48ac815852d3c8
Felipe Almeida <falmeida.1305@gmail.com>
2021-05-27 22:18:05 -0300

    Projeto criado

---
+++ src/main/java/com/serratec/apirestfull/SerratecApplication.java
@@ -0,0 +1,13 @@
+package com.serratec.apirestfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerratecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerratecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/serratec/apirestfull/SerratecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serratec.apirestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerratecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
