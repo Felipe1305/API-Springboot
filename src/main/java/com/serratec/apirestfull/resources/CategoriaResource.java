b8a90a5aac3c5248aca536f2f1c9245571b7144e
Felipe Almeida <falmeida.1305@gmail.com>
2021-05-27 23:09:25 -0300

    Testando Classe de Dominio e Rest

---
+++ src/main/java/com/serratec/apirestfull/resources/CategoriaResource.java
@@ -0,0 +1,28 @@
+package com.serratec.apirestfull.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.serratec.apirestfull.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public List <Categoria> listar() {
+		List<Categoria> lista = new ArrayList<>();
+		
+		Categoria cat1 = new Categoria(1, "Carro");
+		
+		lista.add(cat1);
+		
+		return lista;
+	}
+	
+
+}
