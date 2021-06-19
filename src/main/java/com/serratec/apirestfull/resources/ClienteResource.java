97566fd521ae7ca78b69cc21daaa5c8fe79f12af
Felipe Almeida <falmeida.1305@gmail.com>
2021-06-18 23:20:38 -0300

    atualizações

--- src/main/java/com/serratec/apirestfull/resources/ClienteResource.java
+++ src/main/java/com/serratec/apirestfull/resources/ClienteResource.java
@@ -4,6 +4,7 @@ package com.serratec.apirestfull.resources;
 
 
 import java.net.URI;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,6 +13,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +27,7 @@ import com.serratec.apirestfull.DTO.ClienteNewDTO;
 import com.serratec.apirestfull.domain.Cliente;
 import com.serratec.apirestfull.service.ClienteService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value="/clientes")
 public class ClienteResource {
--- src/main/java/com/serratec/apirestfull/resources/ProdutoResource.java
+++ src/main/java/com/serratec/apirestfull/resources/ProdutoResource.java
@@ -5,21 +5,20 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.serratec.apirestfull.DTO.CategoriaDTO;
 import com.serratec.apirestfull.DTO.ProdutoDTO;
-import com.serratec.apirestfull.domain.Categoria;
 import com.serratec.apirestfull.domain.Produto;
 import com.serratec.apirestfull.resources.utils.URL;
 import com.serratec.apirestfull.service.ProdutoService;
 
 
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/produtos")
 public class ProdutoResource {
