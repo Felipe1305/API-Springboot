5cf0f197d505362d86581f71904f2bb9f468a7d0
Felipe Almeida <falmeida.1305@gmail.com>
2021-06-30 14:55:22 -0300

    Endpoint para buscar cliente por email

--- src/main/java/com/serratec/apirestfull/resources/ClienteResource.java
+++ src/main/java/com/serratec/apirestfull/resources/ClienteResource.java
@@ -40,13 +40,16 @@ public class ClienteResource {
 	
 	@RequestMapping(value="/{id}",method=RequestMethod.GET)
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
-		
-		
 		Cliente obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
-		
-		
 	}
+	
+	@RequestMapping(value="/email",method=RequestMethod.GET)
+	public ResponseEntity<Cliente> findByEmail(@RequestParam(value="value") String email) {
+		Cliente obj = service.findByEmail(email);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 	
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert( @RequestBody ClienteDTO objDTO) {
@@ -63,6 +66,8 @@ public class ClienteResource {
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+	
+	
 
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
